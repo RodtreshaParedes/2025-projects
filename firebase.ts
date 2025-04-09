e4e6c98f0d77fd19ce6ef6a678c775ebfae110a8
Rodtresha Paredes <paredesrodtresha@gmail.com>
2025-04-09 13:45:34 +0800

    Refactor Firebase initialization and add favicon link in document head

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "firebase/app";
+import {getApp, getApps, initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,8 +17,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
 
-export {db, auth};
+export default app;
+export { auth, db };
